eb219b54cbe528c203af6af222ea2e1ddd131818
perco110 <santinopercodani@gmail.com>
2024-10-18 10:39:38 -0300

    agregue comentarios en la clase administrador

--- com/resto/personal/Administrador.java
+++ com/resto/personal/Administrador.java
@@ -32,10 +32,19 @@ public class Administrador extends Personal {
 
 
     @Override
+    // El método `toString()` en la clase `Administrador` está sobrescribiendo el método `toString()` 
+    // predeterminado heredado de la clase `Object`.
+
     public String toString() {
         return "Restaurant administrado: " + getResto();
     }
 
+
+    /**
+     * El método `gestionHorario` gestiona las horas de apertura y cierre de un restaurante tomando la
+     * entrada del usuario para los horarios de apertura y cierre, y actualiza el horario del restaurante
+     * en consecuencia.
+     */
 
     public void gestionHorario(){
 
@@ -63,6 +72,21 @@ public class Administrador extends Personal {
     }
 
 
+    /**
+     * La función "bloquearMesa" bloquea una lista de mesas para una fecha y un rango de tiempo específico,
+     * si están disponibles en ese momento.
+     * 
+     * @param mesasABloquear `mesasABloquear` es una lista de objetos `Mesa` que representan las mesas que
+     * se bloquearán o marcarán como no disponibles para una fecha y rango de tiempo específicos.
+     * @param fecha El parámetro `fecha` representa la fecha en la que las mesas se están bloqueando.
+     * @param horaInicio El parámetro `horaInicio` representa la hora de inicio para la cual la mesa será
+     * bloqueada. Es de tipo `LocalTime`, que es una clase en Java que representa una hora sin fecha ni
+     * zona horaria. Se usa para almacenar la hora del día con una precisión de nanosegundos.
+     * @param horaFin El parámetro `horaFin` representa la hora de finalización para la cual la mesa será
+     * bloqueada. Es de tipo `LocalTime`, que es una clase en Java que representa una hora sin fecha ni
+     * zona horaria. Almacena la hora, el minuto, el segundo y la fracción de segundo.
+     */
+
     public void bloquearMesa(List<Mesa> mesasABloquear, LocalDate fecha, LocalTime horaInicio, LocalTime horaFin) {
         for (Mesa mesa : mesasABloquear) {
             if (mesa.getEstado() == EstadoMesa.Disponible) {
@@ -73,6 +97,15 @@ public class Administrador extends Personal {
             }
         }
     }
+
+    /**
+     * La función `desbloquearMesa` toma una lista de mesas y cambia el estado de cada mesa de
+     * "No_Disponible" a "Disponible" si actualmente no está disponible.
+     * 
+     * @param mesasADesbloquear `mesasADesbloquear` es una lista de objetos `Mesa` que necesitan ser
+     * desbloqueadas. El método `desbloquearMesa` itera sobre esta lista y cambia el estado de cada
+     * objeto `Mesa` de "No_Disponible" a "Disponible".
+     */
 
     public void desbloquearMesa(List<Mesa> mesasADesbloquear) {
         for (Mesa mesa : mesasADesbloquear) {
@@ -85,6 +118,14 @@ public class Administrador extends Personal {
         }
     }
     
+    /**
+     * El método `asignarRol` asigna un rol a un objeto estableciendo el rol mediante el método `setRol`.
+     * 
+     * @param rol El parámetro "rol" en el método "asignarRol" es de tipo "CodRol", que probablemente sea
+     * una clase que representa un rol o permiso en el sistema. El método asigna este rol a la clase
+     * superior usando el método "setRol".
+     */
+
     public void asignarRol(CodRol rol){
         super.setRol(rol); 
     }
