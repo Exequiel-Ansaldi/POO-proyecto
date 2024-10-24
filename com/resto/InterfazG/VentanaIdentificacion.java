d72c3d8181351ed52f332e663c23c8412c928123
Exequiel-Ansaldi <exeansaldi0@gmail.com>
2024-10-23 22:51:51 -0300

    primer ventana hecha

--- com/resto/InterfazG/VentanaIdentificacion.java
+++ com/resto/InterfazG/VentanaIdentificacion.java
@@ -1,4 +1,11 @@
 package com.mycompany.tallerpoo.com.resto.InterfazG;
 
+import javax.swing.*;
+
 public class VentanaIdentificacion {
+    private JPanel panel;
+    private JButton personalButton;
+    private JButton clienteButton;
+    private JLabel Titulo;
+
 }
